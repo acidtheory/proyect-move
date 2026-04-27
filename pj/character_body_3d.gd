7be35934c6784d99797e7562eebde5ff7896b401
RangoAx <alanabichayn3@gmail.com>
2026-04-27 01:15:23 -0300

    introduccion del modelo. la sensibilidad baja al apuntar. mas ajustes al apuntado

--- pj/character_body_3d.gd
+++ pj/character_body_3d.gd
@@ -3,16 +3,18 @@ extends CharacterBody3D
 @onready var _camera := %Camera3D as Camera3D
 @onready var _camera_pivot := %CameraPivot as Node3D
 @onready var _spring_arm := %SpringArm3D as SpringArm3D
-@export var mesh : MeshInstance3D
+@export var mesh : Node3D
 @export_range(0.1, 3.0) var mouse_sensitivity : float = 1.8
-@export var camera_weight : float = 18
+@export var current_mouse_sensitivity : float
+@export var aim_sensitivity : float = mouse_sensitivity/3
+@export var camera_weight : float = 11
 var tilt_limit = deg_to_rad(75)
 var expected_rotation : Vector3 = Vector3(0, 0, 0)
 var expected_velocity : Vector3 = Vector3(0, 0, 0)
-@export var default_length : float = 3.2
-@export var aim_length : float = 2.2
-@export var default_fov : float = 67
-@export var aim_fov : float = 40
+@export var default_length : float = 3
+@export var aim_length : float = 2.3
+@export var default_fov : float = 60
+@export var aim_fov : float = 30
 
 @export var speed = 5.0
 @export var velocity_weight : float = 10
@@ -34,9 +36,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("aim"):
 		_spring_arm.spring_length = move_toward(_spring_arm.spring_length, aim_length, 0.15)
 		_camera.fov = move_toward(_camera.fov, aim_fov, 2.5)
+		current_mouse_sensitivity = aim_sensitivity
 	else:
 		_spring_arm.spring_length = move_toward(_spring_arm.spring_length, default_length, 0.15)
 		_camera.fov = move_toward(_camera.fov, default_fov, 2.5)
+		current_mouse_sensitivity = mouse_sensitivity
 	var input_direction := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := Vector3(input_direction.x,0,input_direction.y).rotated(Vector3(0,1,0),_camera_pivot.rotation.y)
 	_camera_pivot.rotation = _camera_pivot.rotation.move_toward(expected_rotation,delta * camera_weight * _camera_pivot.rotation.distance_to(expected_rotation))
@@ -47,12 +51,12 @@ func _physics_process(delta: float) -> void:
 	expected_velocity.y = velocity.y
 	velocity = velocity.move_toward(expected_velocity, delta * velocity_weight * velocity.distance_to(expected_velocity))
 	#mesh orientation
-	mesh.global_rotation.y = lerp_angle(mesh.global_rotation.y, _camera.global_rotation.y, 0.15)
+	mesh.global_rotation.y = lerp_angle(mesh.global_rotation.y, _camera.global_rotation.y + PI, 0.15)
 	mesh.global_rotation.z = lerp_angle(mesh.global_rotation.z, _camera.global_rotation.z, 0.15)
 	move_and_slide()
 #camera weight
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		expected_rotation.x -= event.screen_relative.y * mouse_sensitivity * 0.001
+		expected_rotation.x -= event.screen_relative.y * current_mouse_sensitivity * 0.001
 		expected_rotation.x = clampf(expected_rotation.x, -tilt_limit, tilt_limit)
-		expected_rotation.y += -event.screen_relative.x * mouse_sensitivity * 0.001
+		expected_rotation.y += -event.screen_relative.x * current_mouse_sensitivity * 0.001
